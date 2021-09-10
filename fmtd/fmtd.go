c119006a22e9b9ad09b33364a449d024f3804dbe
SSSOCPaulCote <paul.cote@sssoc.ca>
2021-09-10 16:26:23 -0400

    created server file

--- fmtd/fmtd.go
+++ fmtd/fmtd.go
@@ -1,7 +1,18 @@
 package fmtd
 
-func Test_fmtd() {
+func main() {
 	config := InitConfig()
 	log := InitLogger(&config)
-	log.Info().Msg("Hello World!")
+	server, err := InitServer(&config, &log)
+	if err != nil {
+		log.Fatal().Msg("Could not initialize server")
+	}
+	err = server.Start()
+	if err != nil {
+		log.Fatal().Msg("Could not start server")
+	}
+	err = server.Stop()
+	if err != nil {
+		log.Fatal().Msg("Could not stop server")
+	}
 }
