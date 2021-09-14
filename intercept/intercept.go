4104bd52434c2c8c8699a56b80c2891c2f912241
SSSOCPaulCote <paul.cote@sssoc.ca>
2021-09-14 12:32:33 -0400

    created grpc proto, compiled, created rpc server, created fmtcli<->grpc interface

---
+++ intercept/intercept.go
@@ -0,0 +1,89 @@
+package intercept
+
+import (
+	"errors"
+	"log"
+	"os"
+	"os/signal"
+	"sync/atomic"
+	"syscall"
+)
+
+var (
+	started int32
+)
+
+// Interceptor is the object controlling application shutdown requests
+type Interceptor struct {
+	interruptChannel chan os.Signal
+	shutdownChannel chan struct{}
+	shutdownRequestChannel chan struct{}
+	quit chan struct{}
+}
+
+// mainInterruptHandler listens for SIGINT (Ctrl+C) signals on the interruptChannel and shutdown requests on the 
+// shutdownRequestChannel. 
+func (interceptor *Interceptor) mainInterruptHandler() {
+	defer atomic.StoreInt32(&started, 0)
+	var isShutdown bool
+	shutdown := func() {
+		if isShutdown {
+			log.Println("Already shutting down...")
+			return
+		}
+		isShutdown = true
+		log.Println("Shutting down...")
+		close(interceptor.quit)
+	}
+	for {
+		select {
+		case signal := <-interceptor.interruptChannel:
+			log.Printf("Received %v", signal)
+			shutdown()
+		case <-interceptor.shutdownRequestChannel:
+			log.Println("Received shutdown request.")
+			shutdown()
+		case <-interceptor.quit:
+			log.Println("Gracefully shutting down.")
+			close(interceptor.shutdownChannel)
+			signal.Stop(interceptor.interruptChannel)
+			return
+		}
+	}
+}
+
+// RequestShutdown initiates a graceful shutdown from the application.
+func (interceptor *Interceptor) RequestShutdown() {
+	select {
+	case interceptor.shutdownRequestChannel <- struct{}{}:
+	case <-interceptor.quit:
+	}
+}
+
+// ShutdownChannel returns the channel that will be closed once the main
+// interrupt handler has exited.
+func (c *Interceptor) ShutdownChannel() <-chan struct{} {
+	return c.shutdownChannel
+}
+
+// InitInterceptor initializes the shutdown and interrupt interceptor
+func InitInterceptor() (Interceptor, error) {
+	if !atomic.CompareAndSwapInt32(&started, 0, 1) {
+		return Interceptor{}, errors.New("Interceptor already initialized")
+	}
+	interceptor := Interceptor{
+		interruptChannel: 		make(chan os.Signal, 1),
+		shutdownChannel:		make(chan struct{}),
+		shutdownRequestChannel:	make(chan struct{}),
+		quit:					make(chan struct{}),
+	}
+	signalsToCatch := []os.Signal{
+		os.Interrupt,
+		os.Kill,
+		syscall.SIGTERM,
+		syscall.SIGQUIT,
+	}
+	signal.Notify(interceptor.interruptChannel, signalsToCatch...)
+	go interceptor.mainInterruptHandler()
+	return interceptor, nil
+}
--- test.go
+++ test.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 	fmtd "github.com/SSSOC-CAN/fmtd/fmtd"
+	"github.com/SSSOC-CAN/fmtd/intercept"
 )
 
 func main() {
+	shutdownInterceptor, err := intercept.InitInterceptor()
 	config := fmtd.InitConfig()
 	log := fmtd.InitLogger(&config)
 	server, err := fmtd.InitServer(&config, &log)
@@ -17,9 +19,19 @@ func main() {
 		log.Fatal().Msg("Could not start server")
 	}
 	log.Debug().Msg(fmt.Sprintf("Server active: %v\tServer stopping: %v", server.Active, server.Stopping))
-	err = server.Stop()
+	grpcServer, err := fmtd.NewGrpcServer(shutdownInterceptor)
 	if err != nil {
-		log.Fatal().Msg("Could not stop server")
+		log.Fatal().Msg("Could not initialize RPC server")
 	}
-	log.Debug().Msg(fmt.Sprintf("Server active: %v\tServer stopping: %v", server.Active, server.Stopping))
+	log.Info().Msg("RPC Server Initialized")
+	err = grpcServer.Start("7777")
+	if err != nil {
+		log.Fatal().Msg("Could not start RPC server")
+	}
+	log.Info().Msg("RPC Server Started")
+	// err = server.Stop()
+	// if err != nil {
+	// 	log.Fatal().Msg("Could not stop server")
+	// }
+	// log.Debug().Msg(fmt.Sprintf("Server active: %v\tServer stopping: %v", server.Active, server.Stopping))
 }
