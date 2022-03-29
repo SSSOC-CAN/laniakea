14523d695fc039cccf92f6bfbfe043934c6894fd
SSSOCPaulCote <paul.cote@sssoc.ca>
2022-03-29 16:39:00 +0000

    new state store changes to demo rga service

--- rga/service_demo.go
+++ rga/service_demo.go
@@ -215,13 +215,9 @@ func (s *RGAService) stopRecording() error {
 //CheckIfBroadcasting listens for a signal from RTD service to either stop or start broadcasting data to it.
 func (s *RGAService) ListenForRTDSignal() {
 	defer s.wgListen.Done()
-	signalChan := make(chan struct{})
-	idx, unsub := s.stateStore.Subscribe(func() {
-		signalChan<-struct{}{}
-	})
+	signalChan, unsub := s.stateStore.Subscribe(s.name)
 	cleanUp := func() {
-		unsub(s.stateStore, idx)
-		close(signalChan)
+		unsub(s.stateStore, s.name)
 	}
 	defer cleanUp()
 	for {
