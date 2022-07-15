57eca0b307b8fdc45a539229b9073b20379126e5
SSSOCPaulCote <paul.cote@sssoc.ca>
2022-07-15 20:31:07 +0000

    added SubscribePluginState rpc method, regenerated stubs

--- fmtrpc/pluginapi.pb.gw.go
+++ fmtrpc/pluginapi.pb.gw.go
@@ -351,6 +351,58 @@ func local_request_PluginAPI_AddPlugin_0(ctx context.Context, marshaler runtime.
 
 }
 
+func request_PluginAPI_GetPlugin_0(ctx context.Context, marshaler runtime.Marshaler, client PluginAPIClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq PluginRequest
+	var metadata runtime.ServerMetadata
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["name"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "name")
+	}
+
+	protoReq.Name, err = runtime.String(val)
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "name", err)
+	}
+
+	msg, err := client.GetPlugin(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_PluginAPI_GetPlugin_0(ctx context.Context, marshaler runtime.Marshaler, server PluginAPIServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq PluginRequest
+	var metadata runtime.ServerMetadata
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["name"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "name")
+	}
+
+	protoReq.Name, err = runtime.String(val)
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "name", err)
+	}
+
+	msg, err := server.GetPlugin(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 // RegisterPluginAPIHandlerServer registers the http handlers for service PluginAPI to "mux".
 // UnaryRPC     :call PluginAPIServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
@@ -506,6 +558,29 @@ func RegisterPluginAPIHandlerServer(ctx context.Context, mux *runtime.ServeMux, 
 		}
 
 		forward_PluginAPI_AddPlugin_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("GET", pattern_PluginAPI_GetPlugin_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/fmtrpc.PluginAPI/GetPlugin", runtime.WithHTTPPathPattern("/v2/plugin/{name}"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_PluginAPI_GetPlugin_0(rctx, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_PluginAPI_GetPlugin_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -710,6 +785,26 @@ func RegisterPluginAPIHandlerClient(ctx context.Context, mux *runtime.ServeMux, 
 
 	})
 
+	mux.Handle("GET", pattern_PluginAPI_GetPlugin_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req, "/fmtrpc.PluginAPI/GetPlugin", runtime.WithHTTPPathPattern("/v2/plugin/{name}"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_PluginAPI_GetPlugin_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_PluginAPI_GetPlugin_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -729,6 +824,8 @@ var (
 	pattern_PluginAPI_ListPlugins_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v2", "plugins"}, ""))
 
 	pattern_PluginAPI_AddPlugin_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v2", "plugin", "add"}, ""))
+
+	pattern_PluginAPI_GetPlugin_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2}, []string{"v2", "plugin", "name"}, ""))
 )
 
 var (
@@ -747,4 +844,6 @@ var (
 	forward_PluginAPI_ListPlugins_0 = runtime.ForwardResponseMessage
 
 	forward_PluginAPI_AddPlugin_0 = runtime.ForwardResponseMessage
+
+	forward_PluginAPI_GetPlugin_0 = runtime.ForwardResponseMessage
 )
