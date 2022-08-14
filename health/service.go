8a121ec3ec4e7f01937dc09beadd09c74bf813fa
SSSOCPaulCote <paul.cote@sssoc.ca>
2022-08-14 13:18:42 +0000

    regenerated protos, added name field to health Responses

--- health/service.go
+++ health/service.go
@@ -2,19 +2,32 @@ package health
 
 import (
 	"context"
+	"time"
 
 	"github.com/SSSOC-CAN/fmtd/fmtrpc"
+	bg "github.com/SSSOCPaulCote/blunderguard"
 	proxy "github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
 )
 
+const (
+	ErrHealthServiceAlreadyRegistered = bg.Error("health service already registered")
+)
+
+var (
+	defaultCheckTimeout time.Duration = 5 * time.Second
+)
+
 type HealthService struct {
 	fmtrpc.UnimplementedHealthServer
+	registeredServices map[string]RegisteredHealthService
 }
 
 // NewHealthService instantiates a new HealthService
 func NewHealthService() *HealthService {
-	return &HealthService{}
+	return &HealthService{
+		registeredServices: make(map[string]RegisteredHealthService),
+	}
 }
 
 // RegisterWithGrpcServer registers the health service with the gRPC server
@@ -30,7 +43,23 @@ func (h *HealthService) RegisterWithRestProxy(ctx context.Context, mux *proxy.Se
 	)
 }
 
+// RegisterHealthService registers a given service that we want to perform health checks on
+func (h *HealthService) RegisterHealthService(name string, s RegisteredHealthService) error {
+	if _, ok := h.registeredServices[name]; ok {
+		return ErrHealthServiceAlreadyRegistered
+	}
+	h.registeredServices[name] = s
+	return nil
+}
+
 // Check is the gRPC command to perform the health check on given API and plugin services
 func (h *HealthService) Check(ctx context.Context, req *fmtrpc.HealthRequest) (*fmtrpc.HealthResponse, error) {
+	if req.Service == "" || req.Service == "all" {
+		// perform health checks on all registered services
+		for _, service := range h.registeredServices {
+			ctx, cancel := context.WithTimeout()
+			service.Ping()
+		}
+	}
 	return nil, nil
 }
